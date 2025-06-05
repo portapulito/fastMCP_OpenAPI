1b91cf1722570e14bb1a38bca45fb2369d1d7f30
Ciu <simonealberucci@gmail.com>
2025-06-05 20:56:25 +0200

    Add files via upload

--- mcp_server.py
+++ mcp_server.py
@@ -6,25 +6,22 @@ API_BASE_URL = "http://localhost:8000"
 
 client = httpx.AsyncClient(base_url=API_BASE_URL)
 
-try:
-    response = httpx.get(f"{API_BASE_URL}/openapi.json")
-    response.raise_for_status()
-    openapi_spec = response.json()
-   
-    print("openapi_spec scaricata con successo")
-    
-    mcp = FastMCP.from_openapi(
-        openapi_spec=openapi_spec,
-        client=client,
-        name="Auto Generated MCP from FastAPI"
-    )
 
-    print("mcp creato con successo")
+openapi_spec = httpx.get("http://localhost:8000/openapi.json").json()
+    
+    
+mcp = FastMCP.from_openapi(
+    openapi_spec=openapi_spec,
+    client=client,
+    name="AI Agent API",
+    all_routes_as_tools=True
+)
+
+print("mcp creato con successo")
 
     
 
-except httpx.HTTPError as e:
-    print(f"Error fetching OpenAPI spec: {e}")
+
 
    
 if __name__ == "__main__":
